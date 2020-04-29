7c6cd13a3db28b7dacc69d9339bd41d94198f76d
gnbarzol <gnbarzol@espol.edu.ec>
2020-04-28 21:43:16 -0500

    Updates

---
+++ precache-manifest.ba9dd1ab33acb306e223cc2f29bcd465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166f88ac84355b2ace2ad3f139690f83",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "1a0757fb8de7e6b0ae88",
+    "url": "/CV/static/css/main.da7a36a1.chunk.css"
+  },
+  {
+    "revision": "5aac864e77da9c91693f",
+    "url": "/CV/static/js/2.f4a14149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV/static/js/2.f4a14149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0757fb8de7e6b0ae88",
+    "url": "/CV/static/js/main.17199ee5.chunk.js"
+  },
+  {
+    "revision": "2ad36d20b001251657aa",
+    "url": "/CV/static/js/runtime-main.71975b18.js"
+  },
+  {
+    "revision": "882e2996fa7bf6217ba69773499bb8f9",
+    "url": "/CV/static/media/avatar.882e2996.jpg"
+  }
+]);
